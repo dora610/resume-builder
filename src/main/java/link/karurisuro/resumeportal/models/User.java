d3fa0f8dce29a23891523d2c2d25b47929ee9fa4
Surajit Karuri <surajitkaruri@gmail.com>
2022-05-29 22:53:13 +0530

    feat: added user and repo

---
+++ src/main/java/link/karurisuro/resumeportal/models/User.java
@@ -0,0 +1,23 @@
+package link.karurisuro.resumeportal.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+
+}
---
+++ src/test/java/link/karurisuro/resumeportal/ResumePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package link.karurisuro.resumeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
