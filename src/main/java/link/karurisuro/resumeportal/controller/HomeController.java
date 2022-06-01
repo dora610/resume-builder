175837dcf94df5701b5c29b293a11aa684803a48
Surajit Karuri <surajitkaruri@gmail.com>
2022-06-01 21:42:11 +0530

    added: profile view with thymleaf

--- src/main/java/link/karurisuro/resumeportal/controller/HomeController.java
+++ src/main/java/link/karurisuro/resumeportal/controller/HomeController.java
@@ -1,7 +1,11 @@
 package link.karurisuro.resumeportal.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class HomeController {
@@ -15,5 +19,4 @@ public class HomeController {
     public String edit() {
         return "Edit page!";
     }
-
 }
